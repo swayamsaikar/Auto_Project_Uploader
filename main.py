d95a00f12a3cdaa3ffead01d61f66c2052b36cde
Swayam <kalyanikar527@gmail.com>
2021-08-22 19:44:12 +0530

    disabled the unecessary logs and added some cool prints

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ ProjectName = input("Enter The Project Name:- \n")
 path = input("Enter The Project Path:- \n")
 commitMessage = str(input("Enter The Commit Message:- \n"))
 
+print("***************Execution Started***********")
+
 # ************* !IMPORTANT READ THIS CAREFULLY
 # set the profile as the default edge account
 # to get your profile path just type in your desidered browser - "Edge://version" and you will get a profile path, so copy that and paste it below
@@ -19,10 +21,14 @@ edge_options.use_chromium = True
 
 #!! Here you set the path of the your profile ending with User Data ****** not the profile folder ****
 edge_options.add_argument(
-    "user-data-dir=C:\\Users\\username\\AppData\\Local\\Microsoft\\Edge\\User Data")  # !Paste The Profile Path here Ignore the Profile with double slasses
+    "user-data-dir=C:\\Users\\Swaya\\AppData\\Local\\Microsoft\\Edge\\User Data")  # !Paste The Profile Path here Ignore the Profile with double slasses
 
 # !! type the name of The Profile folder here
-edge_options.add_argument("profile-directory=Profile 2")
+edge_options.add_argument("profile-directory=Profile 4")
+
+# In the previous commits you may see some unecessary logs by selenium
+# so to disable it we have written below code
+edge_options.add_argument("--disable-logging")
 
 # This code will open Microsoft Edge and set your default account and open github.com
 driver = Edge(options=edge_options, executable_path="msedgedriver.exe")
@@ -35,6 +41,8 @@ driver.maximize_window()
 
 # Below code will open the github website
 driver.get("https://github.com")
+
+print("Github Opened Successfully")
 
 
 def main():
@@ -60,31 +68,52 @@ def main():
         driver.find_element_by_xpath(
             "/html/body/div[4]/main/div/form/div[4]/button").click()
 
+        print(f"Repository Created Successfuly named {ProjectName}")
+
         # then the user will be navigated to the create repository page
         # here first our bot will copy the git link or Https link
         driver.find_element_by_xpath(
             "/html/body/div[4]/div/main/div[2]/div/git-clone-help/div[1]/div/div[4]/div/span/span/clipboard-copy").click()
 
+        print("Successfuly Copied the git link")
+
+        print("Opening CMD")
+
         # this os.system() function will opoen cmd on the given path
         os.system(f"start cmd /K cd {path}")
         time.sleep(1.2)
+
+        print("CMD Opened Successfuly")
 
         # after the opening of the terminal the bot will initialize that folder with git
         keyboard.write("git init")
         keyboard.press_and_release("enter")
 
+        print("Git Initialised Successfuly")
+
         keyboard.write("git add .")
         keyboard.press_and_release("enter")
 
+        print("Arranged Untracked Files")
+        print("Changed To Be Commited")
+
         keyboard.write(f'''git commit -m "{commitMessage}" ''')
         keyboard.press_and_release("enter")
+
+        print("Changed Commited Successfuly")
+        print("Connecting To your Remote repository...")
 
         keyboard.write("git remote add origin ")
         keyboard.press_and_release("control + v")
         keyboard.press_and_release("enter")
 
+        print("Project Connected Successfuly to your remote repository")
+        print("Pushing your project files to github...")
+
         keyboard.write("git push -u origin master")
         keyboard.press_and_release("enter")
+
+        print("congrats project Uploaded Successfuly to github")
 
 
 def login():
@@ -126,6 +155,7 @@ def login():
         "/html/body/div[3]/main/div/div/form/div[3]/button").click()
 
     # **---------- NOW OUR BOT WILL LOGIN TO YOUR ACCOUNT -----------------**
+    print("Account Login Successfull")
     main()
 
 
